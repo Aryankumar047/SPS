fa008bcb70c75bdaf33a9828e9ddf6cb55c3fca3
Aryan Kumar <165165322+Aryankumar047@users.noreply.github.com>
2024-06-01 14:23:36 +0530

    Update SPS_main.py

--- SPS_main.py
+++ SPS_main.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import SPS_home
 import SPS_game
+st.set_page_config(page_title = 'Stone Paper Scissors',
+                    page_icon = ':Controller:',
+                    layout = 'centered',
+                    initial_sidebar_state = 'auto'
+                    )
 dict={"Home":SPS_home,"The Game":SPS_game}
 st.sidebar.title('Navigation')
 ch=st.sidebar.radio('Go to',dict.keys())
