54ee81a55a1416940cfdb932b35cc74f76ba9f13
Aryan Kumar <165165322+Aryankumar047@users.noreply.github.com>
2024-07-23 20:57:01 +0530

    Add files via upload

--- SPS_home.py
+++ SPS_home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 def app():
 	st.header('The Classic Game of Stone Paper Scissors')
-	st.markdown("<p style='font-size:20px'>This web allows you to play the game of stone paper scissors with the computer.</p>",unsafe_allow_html=True)
-	st.markdown('''<p style='color:red;font-size:17px'>{this game is designed by Aryan kumar, Prabhat jha,
+	st.markdown("<p style='font-size:21px'>This web allows you to play the game of stone paper scissors with the computer.</p>",unsafe_allow_html=True)
+	st.markdown('''<p style='color:red;font-size:19px'>{this game is designed by Aryan kumar, Prabhat jha,
 				Pehr Sagar, ifra khan & Vaishnavi Rajput of XII-A}''',unsafe_allow_html=True)
