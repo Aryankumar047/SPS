f141442d95c8c07e1e9be568125eaf3d16da6220
Aryan Kumar <165165322+Aryankumar047@users.noreply.github.com>
2024-07-27 08:18:52 +0530

    Add files via upload

--- SPS_game.py
+++ SPS_game.py
@@ -2,26 +2,15 @@ import streamlit as st
 import random
 def app():
 	st.markdown("<p style='font-size:25px'>Lets Start The Game!</p>",unsafe_allow_html=True)
-	lst=['stone','paper','scissors']
+	lst=[0,1]
 	predicted=str(lst[random.randint(0,2)])
-	if st.button('Stone'):
-		if predicted == 'stone':
-			st.markdown("<p style='font-size:22px'>Its a Tie!</p>",unsafe_allow_html=True)
-		elif predicted == 'paper':
-			st.markdown("<p style='font-size:22px'>Oops! You lost.",unsafe_allow_html=True)
-		elif predicted == 'scissors':
-			st.markdown("<p style='font-size:25px'>Congratulations! You won.",unsafe_allow_html=True)	
-	if st.button('Paper'):
-		if predicted == 'paper':
-			st.markdown("<p style='font-size:22px'>Its a Tie!</p>",unsafe_allow_html=True)
-		elif predicted == 'scissors':
-			st.markdown("<p style='font-size:22px'>Oops! You lost.",unsafe_allow_html=True)
-		elif predicted == 'stone':
-			st.markdown("<p style='font-size:25px'>Congratulations! You won.",unsafe_allow_html=True)
-	if st.button('Scissors'):
-		if predicted == 'scissors':
-			st.markdown("<p style='font-size:22px'>Its a Tie!</p>",unsafe_allow_html=True)
-		elif predicted == 'stone':
-			st.markdown("<p style='font-size:22px'>Oops! You lost.",unsafe_allow_html=True)
-		elif predicted == 'paper':
-			st.markdown("<p style='font-size:25px'>Congratulations! You won.",unsafe_allow_html=True)
+	if st.button('Shoot Yourself'):
+		if predicted == '0':
+			st.markdown("<p style='font-size:22px'>Congratulations! You Won. The Computer Lost.",unsafe_allow_html=True)
+		elif predicted == '1':
+			st.markdown("<p style='font-size:25px'>Oops! You Lost. The Computer Won.",unsafe_allow_html=True)	
+	if st.button('Shoot the computer'):
+		if predicted == '0':
+			st.markdown("<p style='font-size:22px'>Oops! You Lost.The Computer Won",unsafe_allow_html=True)
+		elif predicted == '1':
+			st.markdown("<p style='font-size:25px'>Congratulations! You Won.The Computer Won",unsafe_allow_html=True)
