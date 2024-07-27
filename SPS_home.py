fe34921c978397ab63a1d76ab677c373e25c1a03
Aryan Kumar <165165322+Aryankumar047@users.noreply.github.com>
2024-07-27 08:25:22 +0530

    Add files via upload

--- SPS_home.py
+++ SPS_home.py
@@ -1,5 +1,7 @@
 import streamlit as st
 def app():
 	st.header('The Classic Game of Russian Roullete.')
-	st.markdown("<p style='font-size:21px'>This web allows you to play the game of russian roullete with the computer.</p>",unsafe_allow_html=True)
+	st.markdown('''<p style='font-size:21px'>Russian roullete is a game in which you get a gun with one loaded round and one empty round.
+				This web allows you to play the game of russian roullete with the computer.
+				You have to either shoot yourself or the computer.</p>''',unsafe_allow_html=True)
 	st.markdown('''<p style='color:red;font-size:19px'>{this game is designed by Aryan kumar & Prabhat jha of XII-A}''',unsafe_allow_html=True)
